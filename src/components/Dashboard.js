9540d57e76e59f54c9015356fc092b3b0926bad4
Grant Kayes <glk25@georgetown.edu>
2020-03-25 01:35:21 -0500

    update

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <h1 style={{"color": "#000000"}}> blahj </h1>
+      <div class="card"></div>
+      <div class="card"></div>
+    </div>
+  );
+
+}
+
+export default Dashboard;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Dashboard from './Dashboard';
 import './Login.css';
 
 function Login() {
@@ -7,32 +8,40 @@ function Login() {
   const [loginData, setLoginData] = useState({});
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
   const submit = (data) => {
     setLoginData(data);
     setModalVisible(!modalVisible);
+    setLoggedIn(true);
   }
-  
-  if (!modalVisible) {
-    return (
-      <div className="Login">
-        <button className="login-button" onClick={() => setModalVisible(!modalVisible)}> Log In </button>
-        <button className="login-button" onClick={() => setModalVisible(!modalVisible)}> Sign Up </button>
-      </div>
-    );
+
+  if (!loggedIn) {
+    if (!modalVisible) {
+      return (
+        <div className="Login">
+          <button className="login-button" onClick={() => setModalVisible(!modalVisible)}> Log In </button>
+          <button className="login-button" onClick={() => setModalVisible(!modalVisible)}> Sign Up </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Login">
+        <div className="login-form">
+          <label style={{textAlign: "left"}}>Username</label>
+          <input type="text" onChange={(event) => setUsername(event.target.value)}></input>
+          <label>Password</label>
+          <input type="password"></input>
+          <button onClick={() => submit({"username": username, "password": password})}> Login </button>
+        </div>
+        </div>
+      );
+    }
   } else {
     return (
-      <div className="Login">
-      <div className="login-form">
-        <label style={{textAlign: "left"}}>Username</label>
-        <input type="text" onChange={(event) => setUsername(event.target.value)}></input>
-        <label>Password</label>
-        <input type="password"></input>
-        <button onClick={() => submit({"username": username, "password": password})}> Login </button>
-      </div>
-      </div>
+      <Dashboard/>
     );
   }
-  
+
 }
 
 export default Login;
