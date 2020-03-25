a3825a12c81fa9396ffc745988244da905582c5d
Grant Kayes <glk25@georgetown.edu>
2020-03-25 02:28:39 -0500

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
+import Analytics from './Analytics';
 
 function Dashboard() {
-  return (
-    <div className="Dashboard">
-      <h1 style={{"color": "#000000"}}> blahj </h1>
-      <div class="card"></div>
-      <div class="card"></div>
-    </div>
-  );
+  const [navigateAnalytics, setNavigateAnalytics] = useState(false);
+
+  if (!navigateAnalytics) {
+    return (
+      <div className="Dashboard">
+        <div className="card-wrapper">
+          <h1 style={{"color": "#000000"}}> Analytics </h1>
+          <a href="#"><div class="card"></div></a>
+        </div>
+        <div className="card-wrapper">
+          <h1 style={{"color": "#000000"}}> Recommendation </h1>
+          <a href="#" onClick={() => setNavigateAnalytics(true)}><div class="card"></div></a>
+        </div>     
+        <div className="card-wrapper">
+          <h1 style={{"color": "#000000"}}> Time </h1>
+          <a href="#"><div class="card"></div></a>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div><Analytics/></div>
+    );
+  }
+
 
 }
 
